25180a171b24561211be325ecaa6ab613a0cbd92
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2022-09-21 01:42:39 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice_001/pages/homePage.dart';
 import 'package:flutter_practice_001/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
          
        themeMode: ThemeMode.light,
-       theme: ThemeData(primarySwatch: Colors.blue),
+       theme: ThemeData(primarySwatch: Colors.blue,
+       fontFamily: GoogleFonts.lato().fontFamily,
+      
+       ),
        darkTheme: ThemeData(brightness: Brightness.dark
        ),
 
